40cd1983875b14c2cfce07b22b3dc61e23d34cbb
FlaceyFurr <gontiefox@gmail.com>
2023-04-15 16:02:56 +0300

    Added second section

--- src/components/resultCard/ResultCard.tsx
+++ src/components/resultCard/ResultCard.tsx
@@ -1,7 +1,11 @@
 import "./index.css";
 
 const ResultCard = () => {
-  return <></>;
+  return (
+    <div className="background-result">
+      <h1 className="result">399,981</h1>
+    </div>
+  );
 };
 
 export default ResultCard;
