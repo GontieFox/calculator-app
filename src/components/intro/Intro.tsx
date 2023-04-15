d2989d3a83db94180791b19b1a9835bdbaff8ea0
FlaceyFurr <gontiefox@gmail.com>
2023-04-15 16:09:01 +0300

    Move toggle theme to component

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -1,33 +1,13 @@
-import { useState } from "react";
+import ThemeSwitcher from "../themeSwitcher/ThemeSwitcher";
 import "./index.css";
 
 const Intro = () => {
-  const [value, setValue] = useState(1);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(parseInt(event.target.value));
-  };
-
   return (
     <div className="first-section">
       <h3 className="first-section__title">calc</h3>
       <div className="first-section__theme">
         <span className="first-section__span">theme</span>
-        <div className="switcher">
-          <div className="switcher__section">
-            <span className="switcher__number">1</span>
-            <span className="switcher__number">2</span>
-            <span className="switcher__number">3</span>
-          </div>
-          <input
-            className="switcher__range"
-            type="range"
-            min="1"
-            max="3"
-            value={value}
-            onInput={handleInputChange}
-          />
-        </div>
+        <ThemeSwitcher />
       </div>
     </div>
   );
--- src/components/themeSwitcher/ThemeSwitcher.tsx
+++ src/components/themeSwitcher/ThemeSwitcher.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import "./index.css";
 
 const ThemeSwitcher = () => {
-  return <></>;
+  const [value, setValue] = useState(1);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(parseInt(event.target.value));
+  };
+
+  return (
+    <div className="switcher">
+      <div className="switcher__section">
+        <span className="switcher__number">1</span>
+        <span className="switcher__number">2</span>
+        <span className="switcher__number">3</span>
+      </div>
+      <input
+        className="switcher__range"
+        type="range"
+        min="1"
+        max="3"
+        value={value}
+        onInput={handleInputChange}
+      />
+    </div>
+  );
 };
 
 export default ThemeSwitcher;
