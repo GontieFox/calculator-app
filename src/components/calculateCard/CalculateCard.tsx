ae54dce79fbd964d207ff3c359b731d06954ca60
FlaceyFurr <gontiefox@gmail.com>
2023-04-16 22:21:34 +0300

    Added third section

--- src/components/calculateCard/CalculateCard.tsx
+++ src/components/calculateCard/CalculateCard.tsx
@@ -1,7 +1,32 @@
 import "./index.css";
 
 const CalculateCard = () => {
-  return <></>;
+  return (
+    <div className="card">
+      <div className="card__first-section">
+        <button className="button card__btn">7</button>
+        <button className="button card__btn">8</button>
+        <button className="button card__btn">9</button>
+        <button className="button card__delete-btn">DEL</button>
+        <button className="button card__btn">4</button>
+        <button className="button card__btn">5</button>
+        <button className="button card__btn">6</button>
+        <button className="button card__btn">+</button>
+        <button className="button card__btn">1</button>
+        <button className="button card__btn">2</button>
+        <button className="button card__btn">3</button>
+        <button className="button card__btn">-</button>
+        <button className="button card__btn">.</button>
+        <button className="button card__btn">0</button>
+        <button className="button card__btn">/</button>
+        <button className="button card__btn">x</button>
+      </div>
+      <div className="card__second-section">
+        <button className="button card__reset-btn">RESET</button>
+        <button className="button card__result-btn">=</button>
+      </div>
+    </div>
+  );
 };
 
 export default CalculateCard;
